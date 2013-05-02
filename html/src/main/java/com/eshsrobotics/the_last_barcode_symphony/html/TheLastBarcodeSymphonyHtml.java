8396f6f394e2efeea3cc91f520ad60908d7c04cd
Zachary Latta <zchlatta@gmail.com>
2013-05-01 21:19:56 -0700

    Added generated Maven files.

---
+++ html/src/main/java/com/eshsrobotics/the_last_barcode_symphony/html/TheLastBarcodeSymphonyHtml.java
@@ -0,0 +1,19 @@
+package com.eshsrobotics.the_last_barcode_symphony.html;
+
+import com.eshsrobotics.the_last_barcode_symphony.core.TheLastBarcodeSymphony;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class TheLastBarcodeSymphonyHtml extends GwtApplication {
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new TheLastBarcodeSymphony();
+	}
+	
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		return new GwtApplicationConfiguration(480, 320);
+	}
+}
