985704fab2d8c50e2c09c103cd9002c741b49a94
luke738 <luke7380@gmail.com>
2013-05-01 22:21:52 -0700

    Closed Issue #1 and Issue #2: Draws a sine of sprites and a square on the screen.

--- core/src/main/java/com/eshsrobotics/the_last_barcode_symphony/core/TheLastBarcodeSymphony.java
+++ core/src/main/java/com/eshsrobotics/the_last_barcode_symphony/core/TheLastBarcodeSymphony.java
@@ -1,50 +1,65 @@
-package com.eshsrobotics.the_last_barcode_symphony.core;
+package com.eshsrobotics.the_last_barcode_symphony;
 
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
-public class TheLastBarcodeSymphony implements ApplicationListener
-{
-    Texture texture;
-    SpriteBatch batch;
-
+public class TheLastBarcodeSymphonyGame implements ApplicationListener {
+    private SpriteBatch batch;
+    private Texture texture;
+    private ShapeRenderer shape;
+    
     @Override
-    public void create()
-    {
-        texture = new Texture(Gdx.files.internal("libgdx-logo.png"));
+    public void create() 
+    {        
+        texture = new Texture(Gdx.files.internal("Test.png"));
         batch = new SpriteBatch();
+        shape = new ShapeRenderer();
     }
 
     @Override
-    public void resize(int width, int height)
+    public void dispose() 
     {
+
     }
 
     @Override
-    public void render()
-    {
-        Gdx.gl.glClearColor(0, 0, 0, 0);
-        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(texture, 100, 100);
-        batch.end();
+    public void render() {        
+        Gdx.gl.glClearColor(1, 1, 1, 1); //White Screen
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT); // Clear screen
+        
+        for(int i = 0; i < 10; i++)
+        {
+        	batch.begin();
+        	batch.draw(texture, (float) (16*Math.sin(i) + 16), 16*i);
+        	batch.end();
+        }
+        
+        shape.begin(ShapeType.Filled);
+        shape.setColor(0,0,0,1);
+        shape.rect(100, 100, 50, 50);
+        shape.end();
+        
     }
 
     @Override
-    public void pause()
-    {
+    public void resize(int width, int height) {
     }
 
     @Override
-    public void resume()
-    {
+    public void pause() {
     }
 
     @Override
-    public void dispose()
-    {
+    public void resume() {
     }
 }
