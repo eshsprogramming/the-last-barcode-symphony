ff1fe7ca08748c33de12bff593efe94640302664
luke738 <luke7380@gmail.com>
2013-05-01 22:31:25 -0700

    Removed extraneous imports

--- core/src/main/java/com/eshsrobotics/the_last_barcode_symphony/core/TheLastBarcodeSymphony.java
+++ core/src/main/java/com/eshsrobotics/the_last_barcode_symphony/core/TheLastBarcodeSymphony.java
@@ -2,14 +2,9 @@ package com.eshsrobotics.the_last_barcode_symphony.core;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
