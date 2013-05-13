682b2b42a6f1bdbf0ea807396aabe60d0219fc52
Luke St. Regis <luke7380@gmail.com>
2013-05-13 09:42:28 -0700

    Added 3, randomly placed and sized, rectangles moving from left to right at different speeds.

--- core/src/main/java/com/eshsrobotics/the_last_barcode_symphony/core/Shape.java
+++ core/src/main/java/com/eshsrobotics/the_last_barcode_symphony/core/Shape.java
@@ -12,10 +12,20 @@ public class Shape implements InputProcessor
     float shapeRed = 0;
     float shapeGreen = 0;
     float shapeBlue = 0;
+    int height = Gdx.graphics.getHeight();
+    int width = Gdx.graphics.getWidth();
     int shapeX = 0;
-    int shapeY = 0;
-    int shapeWidth = 50;
-    int shapeHeight = 50;
+    int shapeY = (int) (Math.random()*height);
+    int shapeWidth = (int) (Math.random()*250);
+    int shapeHeight = (int) (Math.random()*250);
+    int shapeXa = 0;
+    int shapeYa = (int) (Math.random()*height);
+    int shapeWidtha = (int) (Math.random()*250);
+    int shapeHeighta = (int) (Math.random()*250);
+    int shapeXb = 0;
+    int shapeYb = (int) (Math.random()*height);
+    int shapeWidthb = (int) (Math.random()*250);
+    int shapeHeightb = (int) (Math.random()*250);
     
     public void create()
     {
@@ -24,53 +34,39 @@ public class Shape implements InputProcessor
     
     public void render(float delta)
     {
+        shapeX += 2;
+        shapeXa += 3;
+        shapeXb += 4;
         shape.begin(ShapeType.Filled);
         shape.setColor(shapeRed, shapeGreen, shapeBlue, 1);
         shape.rect(shapeX, shapeY, shapeWidth, shapeHeight);
-        shape.rect(300, 0, 25, Gdx.graphics.getHeight());
+        shape.rect(shapeXa, shapeYa, shapeWidtha, shapeHeighta);
+        shape.rect(shapeXb, shapeYb, shapeWidthb, shapeHeightb);
         shape.end();
-        //Controls size with "WASD"
-        if(Gdx.input.isKeyPressed(Keys.W))
+        if (shapeX > width)
         {
-            shapeHeight = shapeHeight + 3;
+            shapeX = 0;
+            shapeY = (int) (Math.random()*height);
+            shapeHeight = (int) (Math.random()*250);
+            shapeWidth = (int) (Math.random()*250);
         }
-
-        if(Gdx.input.isKeyPressed(Keys.S))
+        
+        if (shapeXa > width)
         {
-            shapeHeight = shapeHeight - 3;
+            shapeXa = 0;
+            shapeYa = (int) (Math.random()*height);
+            shapeHeighta = (int) (Math.random()*250);
+            shapeWidtha = (int) (Math.random()*250);
         }
-
-        if(Gdx.input.isKeyPressed(Keys.A))
+        
+        if (shapeXb > width)
         {
-            shapeWidth = shapeWidth - 3;
+            shapeXb = 0;
+            shapeYb = (int) (Math.random()*height);
+            shapeWidthb = (int) (Math.random()*250);
+            shapeHeightb = (int) (Math.random()*250);
         }
-
-        if(Gdx.input.isKeyPressed(Keys.D))
-        {
-            shapeWidth = shapeWidth + 3;
-        }
-
-        //Controls Location with arrow keys
-        if(Gdx.input.isKeyPressed(Keys.DPAD_UP))
-        {
-            shapeY = shapeY + 3;
-        }
-
-        if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN))
-        {
-            shapeY = shapeY - 3;
-        }
-
-        if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT))
-        {
-            shapeX = shapeX - 3;
-        }
-
-        if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT))
-        {
-            shapeX = shapeX + 3;
-        }
-
+        
         //Poorly controls color, will be fixed in future. Use numbers 1-3 to control
         if(Gdx.input.isKeyPressed(Keys.NUM_1))
         {
